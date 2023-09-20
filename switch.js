70aee63ab6b56027f0f09b1f492bfb270b02a96a
nachthelad <nachthelad@gmail.com>
2023-09-20 20:22:46 -0300

    Updated components with Bootstrap

--- switch.js
+++ switch.js
@@ -1,39 +1,24 @@
-/* js para el switch */
-const themeSwitch = document.getElementById("themeSwitch");
-const themeChangeElements = document.querySelectorAll(".theme-change");
-const inputElements = document.querySelectorAll("input[type=text], input[type=number], select");
-const buttonElement = document.querySelector('#addButton');
-const originalInputStyles = [];
+document.addEventListener("DOMContentLoaded", function() {
+  const themeSwitch = document.getElementById('themeSwitch');
+  const faSun = document.querySelector('.fa-sun');
+  const faMoon = document.querySelector('.fa-moon');
 
-for (const input of inputElements) {
-  originalInputStyles.push({
-    background: input.style.backgroundColor,
-    color: input.style.color
-  });
-}
-
-themeSwitch.addEventListener("change", () => {
-  if (themeSwitch.checked) {
-    document.body.style.backgroundColor = "black";
-    document.body.style.color = "white";
-    buttonElement.classList.add("theme-dark");
-    themeChangeElements.forEach(element => {
-      element.style.backgroundColor = "#333";
-    });
-    inputElements.forEach((input) => {
-      input.style.backgroundColor = "#333";
-      input.style.color = "white";
-    });
-  } else {
-    document.body.style.backgroundColor = "white";
-    document.body.style.color = "black";
-    buttonElement.classList.remove("theme-dark");
-    themeChangeElements.forEach(element => {
-      element.style.backgroundColor = "white"; 
-    });
-    inputElements.forEach((input, index) => {
-      input.style.backgroundColor = originalInputStyles[index].background;
-      input.style.color = originalInputStyles[index].color; 
-    });
+  const currentTheme = localStorage.getItem('theme');
+  if (currentTheme) {
+    document.documentElement.setAttribute('data-bs-theme', currentTheme);
   }
+
+  themeSwitch.addEventListener('change', function() {
+    if (this.checked) {
+      document.documentElement.setAttribute('data-bs-theme', 'dark');
+      localStorage.setItem('theme', 'dark');
+      faSun.style.color = '#fff'
+      faMoon.style.color = '#fff'
+    } else {
+      document.documentElement.setAttribute('data-bs-theme', 'light');
+      localStorage.setItem('theme', 'light');
+      faSun.style.color = '#000'
+      faMoon.style.color = '#000'
+    }
+  });
 });
